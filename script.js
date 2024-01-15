9a98e5065775936bf8729dd1a46bdf190072031f
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-15 22:09:18 +0100

    Create canvas for painting

---
+++ script.js
@@ -0,0 +1,8 @@
+
+for (i=0; i < 1023; i++) {
+    const canvas = document.createElement('div');
+    canvas.setAttribute('class', "canvasChild");
+    canvasContainer.appendChild(canvas);
+    }
+
+
