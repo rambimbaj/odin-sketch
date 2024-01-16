06816317d21c82b7fa6f874e02040621f7605bd1
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-16 13:05:10 +0100

    Add mouseover function to canvas

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 for (i=0; i < 1023; i++) {
     const canvas = document.createElement('div');
+    canvas.onmouseover = function () {
+        canvas.style.backgroundColor = 'brown';
+        }
     canvas.setAttribute('class', "canvasChild");
     canvasContainer.appendChild(canvas);
     }
-
 
