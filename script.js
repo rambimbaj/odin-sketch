917dc68a974b449c702f7ae723ddc1ec459b1e3a
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-19 22:28:47 +0100

    Add progressive darkening effect

--- script.js
+++ script.js
@@ -5,22 +5,39 @@ function createCanvas(size) {
     let b = Math.floor(Math.random() * 256);
     return "rgb(" + r + "," + g + "," + b + ")";
   }
-
   for (i = 0; i < size; i++) {
     const canvasRow = document.createElement("div");
     canvasRow.setAttribute("class", "canvasRow");
+    canvasRow.style.width = `calc(100% / ${size}`;
+    canvasRow.style.height = `calc(100% / ${size}`;
     canvasRow.onmouseover = function () {
       canvasRow.style.backgroundColor = randomColor();
+      let rowOpacity = Number(canvasRow.style.opacity);
+      canvasRow.style.opacity = rowOpacity + 0.1;
+      return canvasRow;
     };
     canvasContainer.appendChild(canvasRow);
   }
   for (k = 1; k <= size; k++) {
     const canvasColumn = document.createElement("div");
     canvasColumn.setAttribute("class", "canvasColumn");
+    canvasColumn.style.width = `calc(100% / ${size}`;
+    canvasColumn.style.height = `calc(100% / ${size}`;
     canvasColumn.onmouseover = function () {
       canvasColumn.style.backgroundColor = randomColor();
+      let columnOpacity = Number(canvasColumn.style.opacity);
+      canvasColumn.style.opacity = columnOpacity + 0.1;
+      return canvasColumn;
     };
     canvasContainer.appendChild(canvasColumn);
   }
 }
-createCanvas(5);
+function changeSize() {
+  let size = prompt("What size shall it be?", 16);
+  if (size > 1000 || size < 0) {
+    return prompt("0-1000 only!");
+  }
+  return size;
+}
+document.getElementById("canvasSize").addEventListener("click", changeSize);
+createCanvas(changeSize());
