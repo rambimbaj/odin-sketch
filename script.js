f924f2dcc18b4a68c656e1192d4753decd9e388c
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-19 19:40:07 +0100

    Add random color element

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+function createCanvas(size) {
+  function randomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
 
-for (i=0; i < 1023; i++) {
-    const canvas = document.createElement('div');
-    canvas.onmouseover = function () {
-        canvas.style.backgroundColor = 'brown';
-        }
-    canvas.setAttribute('class', "canvasChild");
-    canvasContainer.appendChild(canvas);
-    }
-
+  for (i = 0; i < size; i++) {
+    const canvasRow = document.createElement("div");
+    canvasRow.setAttribute("class", "canvasRow");
+    canvasRow.onmouseover = function () {
+      canvasRow.style.backgroundColor = randomColor();
+    };
+    canvasContainer.appendChild(canvasRow);
+  }
+  for (k = 1; k <= size; k++) {
+    const canvasColumn = document.createElement("div");
+    canvasColumn.setAttribute("class", "canvasColumn");
+    canvasColumn.onmouseover = function () {
+      canvasColumn.style.backgroundColor = randomColor();
+    };
+    canvasContainer.appendChild(canvasColumn);
+  }
+}
+createCanvas(5);
