879545e9c7373b129ec244167a41f456980feb4b
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-20 15:33:53 +0100

    Fix canvas box generation

--- script.js
+++ script.js
@@ -5,39 +5,27 @@ function createCanvas(size) {
     let b = Math.floor(Math.random() * 256);
     return "rgb(" + r + "," + g + "," + b + ")";
   }
-  for (i = 0; i < size; i++) {
-    const canvasRow = document.createElement("div");
-    canvasRow.setAttribute("class", "canvasRow");
-    canvasRow.style.width = `calc(100% / ${size}`;
-    canvasRow.style.height = `calc(100% / ${size}`;
-    canvasRow.onmouseover = function () {
-      canvasRow.style.backgroundColor = randomColor();
-      let rowOpacity = Number(canvasRow.style.opacity);
-      canvasRow.style.opacity = rowOpacity + 0.1;
-      return canvasRow;
+
+  for (i = 0; i < size ** 2; i++) {
+    const canvas = document.createElement("div");
+    canvas.setAttribute("class", "canvas");
+    canvas.style.flexBasis = 600 / size + "px";
+    canvas.onmouseover = function () {
+      canvas.style.backgroundColor = randomColor();
+      let canvasOpacity = Number(canvas.style.opacity);
+      canvas.style.opacity = canvasOpacity + 0.1;
+      return canvas;
     };
-    canvasContainer.appendChild(canvasRow);
-  }
-  for (k = 1; k <= size; k++) {
-    const canvasColumn = document.createElement("div");
-    canvasColumn.setAttribute("class", "canvasColumn");
-    canvasColumn.style.width = `calc(100% / ${size}`;
-    canvasColumn.style.height = `calc(100% / ${size}`;
-    canvasColumn.onmouseover = function () {
-      canvasColumn.style.backgroundColor = randomColor();
-      let columnOpacity = Number(canvasColumn.style.opacity);
-      canvasColumn.style.opacity = columnOpacity + 0.1;
-      return canvasColumn;
-    };
-    canvasContainer.appendChild(canvasColumn);
+    canvasContainer.appendChild(canvas);
   }
 }
 function changeSize() {
-  let size = prompt("What size shall it be?", 16);
-  if (size > 1000 || size < 0) {
-    return prompt("0-1000 only!");
+  let size = prompt("What size shall it be?", 8);
+  if (size > 100 || size < 0 || isNaN(size)) {
+    return alert("0-100 only!");
   }
   return size;
 }
-document.getElementById("canvasSize").addEventListener("click", changeSize);
-createCanvas(changeSize());
+
+// add canvas reset to button
+// make proper grid
