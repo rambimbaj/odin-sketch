e1ef02ac13ecdab9c90537901d5bb52b03475924
rambimbaj <107889088+rambimbaj@users.noreply.github.com>
2024-01-20 19:48:24 +0100

    Add clear function on resize

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function createCanvas(size) {
+  const clear = document.getElementById("canvasContainer");
+  while (clear.hasChildNodes()) {
+    clear.removeChild(clear.firstChild);
+  }
   function randomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     return "rgb(" + r + "," + g + "," + b + ")";
   }
-
   for (i = 0; i < size ** 2; i++) {
     const canvas = document.createElement("div");
     canvas.setAttribute("class", "canvas");
@@ -20,12 +23,9 @@ function createCanvas(size) {
   }
 }
 function changeSize() {
-  let size = prompt("What size shall it be?", 8);
+  let size = prompt("What size shall it be? 0-100", 8);
   if (size > 100 || size < 0 || isNaN(size)) {
     return alert("0-100 only!");
   }
   return size;
 }
-
-// add canvas reset to button
-// make proper grid
